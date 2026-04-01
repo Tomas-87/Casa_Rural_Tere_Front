6a22a1aa590e7d29a1525b670cb396d15d4ba9a8
Tomas <tomascarrerasalbesa@gmail.com>
2026-04-01 09:53:10 +0200

    arreglando telefono

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,18 +5,19 @@ const Footer = ({ casa }) => {
   const location = useLocation();
 
   const isImagenes = location.pathname === "/imagenes";
+  const phone = import.meta.env.VITE_PHONE;
 
   return (
     <footer className={isImagenes ? styles.footerImagenes : styles.footerHome}>
       <div>
         <p className="hover">
           Telefono:
-          <a href={`tel:${casa?.telefono}`}>Contactar</a>
+          <a href={`tel:${phone}`}>Contactar</a>
         </p>
         <p>Calle Lepanto Nº2 Valderrobres</p>
         <p className="hover">
           WhatsApp:
-          <a href="https://wa.me/34612345678">Contactar</a>
+          <a href={`https://wa.me/${phone}`}>Contactar</a>
         </p>
       </div>
       <p>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,16 +5,14 @@ export default function Header({ casa }) {
   const location = useLocation();
 
   const isImagenes = location.pathname === "/imagenes";
+  const phone = import.meta.env.VITE_PHONE;
 
   return (
     <header className={isImagenes ? styles.headerImagenes : styles.headerHome}>
       <div className={styles.top}>
         <p className={styles.nombre}>Casa Rural en Valderrobres</p>
 
-        <a
-          href={`tel:${casa?.telefono}`}
-          className={`${styles.telefono} hover`}
-        >
+        <a href={`tel:${phone}`} className={`${styles.telefono} hover`}>
           (+34) 669 25 38 38 <span>Llamar</span>
         </a>
       </div>
